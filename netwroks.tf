4c32f1b7ee2dad5d4c74df66d43a07b0a0048b8b
cmrh3 <81811983+cmrh3@users.noreply.github.com>
2023-06-02 12:13:15 -0400

    Tests str replacement at name level

--- netwroks.tf
+++ netwroks.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "vnet" {
-  name                = "replace(${var.vnet_name} ${var.ARM_RG_NAME}, " ", "-")"
+  name                = "${var.vnet_name}_${replace(var.ARM_RG_NAME," ","_")}"
   location            = var.devops_sb_resource_group_location
   resource_group_name = var.ARM_RG_ID
   address_space       = ["10.0.0.0/16"]
