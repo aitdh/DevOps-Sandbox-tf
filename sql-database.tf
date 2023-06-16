cf9058e1c78ea90c7699614a4af733495e89a75c
cmrh3 <81811983+cmrh3@users.noreply.github.com>
2023-06-16 14:47:05 -0400

    Updates db name

--- sql-database.tf
+++ sql-database.tf
@@ -7,8 +7,8 @@ resource "azurerm_mssql_server" "dummy_sql_db_server" {
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
 }
 
-resource "azurerm_mssql_database" "dummy_sql_db_server" {
-  name           = "${var.devops_sb_resource_group_location}-${var.environment}-sql-server"
+resource "azurerm_mssql_database" "dummy_sql_db" {
+  name           = "${var.devops_sb_resource_group_location}-${var.environment}-sqldb"
   server_id      = azurerm_mssql_server.dummy_sql_db_server.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
