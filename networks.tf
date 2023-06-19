08d807938974554fd032886d58767e9a26189117
cmrh3 <81811983+cmrh3@users.noreply.github.com>
2023-06-19 12:14:31 -0400

    Tries different things

--- networks.tf
+++ networks.tf
@@ -3,8 +3,6 @@ resource "azurerm_virtual_network" "vnet" {
   location            = var.devops_sb_resource_group_location
   resource_group_name = var.ARM_RG_NAME
   address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
-#  vnet_subnet_id      = data.azurerm_subnet.aks_sbn_data.id
 
   #  subnet {
   #    name           = "snet-appcs"
