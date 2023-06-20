4d68b5a194b4652fe611cf2b04cf32553130be10
cmrh3 <81811983+cmrh3@users.noreply.github.com>
2023-06-20 16:20:42 -0400

    Updates Container Registry Name

--- container-registry.tf
+++ container-registry.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr" {
-  name                = "containerRegistry1"
+  name                = "acr_${replace(var.ARM_RG_NAME,"-","_")}"
   resource_group_name = var.ARM_RG_NAME
   location            = var.devops_sb_resource_group_location
   sku                 = "Basic"
