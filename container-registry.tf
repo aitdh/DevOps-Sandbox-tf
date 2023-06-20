acac982065d647411908193493a3a0a15c28dcc1
cmrh3 <81811983+cmrh3@users.noreply.github.com>
2023-06-20 16:43:42 -0400

    Adds managed identity to ACR

--- container-registry.tf
+++ container-registry.tf
@@ -4,4 +4,18 @@ resource "azurerm_container_registry" "acr" {
   location            = var.devops_sb_resource_group_location
   sku                 = "Basic"
   admin_enabled       = false
+
+  identity {
+    type = "UserAssigned"
+    identity_ids = [
+      azurerm_user_assigned_identity.acr_mngd_id.id
+    ]
+  }
+}
+
+resource "azurerm_user_assigned_identity" "acr_mngd_id" {
+  resource_group_name = var.ARM_RG_NAME
+  location            = var.devops_sb_resource_group_location
+
+  name = "dh-devops-registry-uai"
 }
