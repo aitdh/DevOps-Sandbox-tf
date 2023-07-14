28f63eda4e9f2804b40eaf7ec6dd7369f4c7ce75
cmrh3 <81811983+cmrh3@users.noreply.github.com>
2023-07-14 16:22:31 -0400

    Updates role definition for ACR's SP

--- container-registry.tf
+++ container-registry.tf
@@ -53,7 +53,7 @@ resource "azurerm_container_registry" "acr" {
 #https://learn.microsoft.com/en-us/azure/role-based-access-control/built-in-roles?WT.mc_id=AZ-MVP-5004151
 resource "azurerm_role_assignment" "role_acr_contributor_assign" {
   scope                = azurerm_container_registry.acr.id
-  role_definition_name = "Custom AcrContributor ${var.environment}"
+  role_definition_name = "AcrImageSigner"
   principal_id         = var.SP_ACR_OBJ_ID
   # depends_on           = [azurerm_role_definition.role_acr_contributor]
 }
